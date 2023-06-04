7556d490672e574738289d4700ad4d53bf406d59
Michael <cj_gong@163.com>
2023-06-04 21:22:19 +1000

    Update Home page with Categories and CategoryItem components.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Categories from './components/Categories'
+
+const App = () => {
+  return (
+    <Categories
+
+    />
+    // <div>
+    //   <div className="categories-container">
+    //     {categories.map((item) =>
+    //       <CategoryItem
+    //         key={item.id}
+    //         category={item}
+    //       />)
+    //     }
+    //   </div>
+    // </div>
+  )
+}
+
+export default App
