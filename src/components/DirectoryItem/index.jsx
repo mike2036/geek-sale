50a106bfa6f2666b3feebd48c1e746fdc8cd6f3c
Michael <gong.changbo@outlook.com>
2023-07-08 19:27:51 +1000

    fixed jump on home page

--- src/components/DirectoryItem/index.jsx
+++ src/components/DirectoryItem/index.jsx
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 const DirectoryItem = ({ key, category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className="directory-item-container">
-      <Link to={`/shop/${title}`}></Link>
+    <Link className="directory-item-container" to={`/shop/${title}`}>
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="category-body-container">
         <h2>{title}</h2>
         <p>SHOP NOW</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
