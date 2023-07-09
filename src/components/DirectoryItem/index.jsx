ccb04590f84a92e99e08218ad6328bd518356d81
Michael <gong.changbo@outlook.com>
2023-07-09 11:33:08 +1000

    optimized DirectoryItem by replacing Link tag with useNavigate hook

--- src/components/DirectoryItem/index.jsx
+++ src/components/DirectoryItem/index.jsx
@@ -1,16 +1,20 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const DirectoryItem = ({ key, category }) => {
-  const { imageUrl, title } = category;
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <Link className="directory-item-container" to={`/shop/${title}`}>
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="category-body-container">
         <h2>{title}</h2>
         <p>SHOP NOW</p>
       </div>
-    </Link>
+    </div>
   );
 };
 
